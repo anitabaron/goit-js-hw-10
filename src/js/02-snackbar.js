48c6359adac6afaa2211500bd5a9fe02a403d062
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-09 18:21:01 +0200

    add js source

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -2,3 +2,8 @@
 import iziToast from 'izitoast';
 // Kolejny import stylów
 import 'izitoast/dist/css/iziToast.min.css';
+
+iziToast.show({
+  title: 'Hey',
+  message: 'What would you like to add?',
+});
