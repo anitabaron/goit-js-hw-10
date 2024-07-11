6162226ed5a709a5b439e1165234f39cecb514bd
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-11 15:52:08 +0200

    update files html

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -2,6 +2,10 @@
 import flatpickr from 'flatpickr';
 // Dodatkowy import stylów
 import 'flatpickr/dist/flatpickr.min.css';
+// Opisany w dokumentacji
+import iziToast from 'izitoast';
+// Kolejny import stylów
+import 'izitoast/dist/css/iziToast.min.css';
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -25,3 +29,29 @@ function convertMs(ms) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+let userSelectedDate;
+
+const flatpickr = (selector, options) => {
+  window.alert(`Please choose a date in the future`);
+};
+
+iziToast.show({
+  title: 'Hey',
+  message: 'Please choose a date in the future',
+});
+
+const addLeadingZero = value => {
+  [padStart()];
+  // (< `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart` >)
+};
--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,9 +1,1 @@
-// Opisany w dokumentacji
-import iziToast from 'izitoast';
-// Kolejny import stylów
-import 'izitoast/dist/css/iziToast.min.css';
 
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
